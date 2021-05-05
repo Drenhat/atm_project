06483edb462ed87cf8a08ff6e4f3df2378b01788
Tim Huerne <timothee.huerne@gmail.com>
2021-05-04 21:30:32 -0400

    first commit of the file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import './App.css';
 
-function App() {
+const ATMDeposit = ({ onChange, isDeposit, validTransaction}) => {
+  let [isValid] = [validTransaction];
+  const choice = ['Deposit', 'Cash Back'];
+  console.log(`ATM isDeposit: ${isDeposit}`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="menu-select"> {choice[Number(!isDeposit)]}</section>
+      <label className="select-number">Select the amount</label>
+      <input id="number-input" type="number" width="200" onChange={onChange}></input>  
+      <br/>
+      <Button className="atm-button" variant="primary" type="submit" value="Submit" id="submit-input" disabled={!isValid}>
+        Submit
+      </Button>
+      {/* <input type="submit" width="200" value="Submit" id="submit-input" disabled={!isValid}></input> */}
+    </>
   );
-}
+};
 
-export default App;
+const Account = () => {
+  const [deposit, setDeposit] = React.useState(0);
+  const [totalState, setTotalState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+  const [atmMode, setAtmMode] = React.useState(``)
+  const [validTransaction, setValidTransaction] = React.useState(false);
+
+  let status = `Account Balance $ ${totalState} `;
+  console.log(`Account Rendered with isDeposit: ${isDeposit}`);
+
+  const handleChange = (event) => {
+    console.log(`handleChange ${event.target.value}`);
+    setDeposit(Number(event.target.value));
+    setValidTransaction(false)
+    if (event.target.value <= 0) {return}
+    if (atmMode == `Cash Back` && event.target.value > totalState){
+      setValidTransaction(false);
+    }
+    else {setValidTransaction(true)}
+  };
+  const handleSubmit = (event) => {
+    let newTotal = isDeposit ? totalState + deposit : totalState - deposit;
+    setTotalState(newTotal);
+    setValidTransaction(false);
+    event.preventDefault();
+  };
+
+  const handleModeSelect = (event) => {
+    setAtmMode(event.target.value);
+    if (event.target.value == `Deposit`) setIsDeposit(true);
+    if (event.target.value == `Cash Back`) setIsDeposit(false); 
+  } 
+
+  return (
+    <form className="atm-form" onSubmit={handleSubmit}>
+      <h2 id="total">{status}</h2>
+      <label className="select-action">Select an action below to continue</label>
+      <Form.Control as="select" size="lg" className="form-select" aria-label="Default select example" onChange={(e) => handleModeSelect(e)} name="mode" id="mode-select">
+        <option id="no-selection" value=""></option>
+        <option id="deposit-selection" value="Deposit">Deposit</option>
+        <option id="cashback-selection" value="Cash Back">Cash Back</option>
+      </Form.Control>
+      <br />
+      {
+        atmMode && <ATMDeposit onChange={handleChange} isDeposit={isDeposit}
+        validTransaction={validTransaction}></ATMDeposit>
+      }
+    </form>
+  );
+};
+// ========================================
+ReactDOM.render(<Account />, document.getElementById('root'));
+
+
+export default Account;
